43dfd3e8cec0fcff950ba06633a262dde0785d11
Aqab-pervaiz <meaqab@gmail.com>
2024-11-19 16:58:26 +0500

    Git hub actions practice

--- flights/admin.py
+++ flights/admin.py
@@ -1,15 +1,17 @@
 from django.contrib import admin
-from .models import Airports,Flight,Passenger
+from .models import Airports, Flight, Passenger
 
 
 # Register your models here.
 
 class FlightAdmin(admin.ModelAdmin):
-    list_display=["id","destination","origin"]
+    list_display = ["id", "destination", "origin"]
+
 
 class PassengerAdmin(admin.ModelAdmin):
-    filter_horizontal=("flights",)
+    filter_horizontal = ("flights",)
+
 
 admin.site.register(Airports)
-admin.site.register(Flight,FlightAdmin)
+admin.site.register(Flight, FlightAdmin)
 admin.site.register(Passenger, PassengerAdmin)
